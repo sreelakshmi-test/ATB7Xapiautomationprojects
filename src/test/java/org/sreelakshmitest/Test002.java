fc361f7da61356b1b36650e5bed5dbb61e7f3801
sreelakshmi <sreelakshmip862@gmail.com>
2025-07-08 22:19:26 +0530

    test002 added

--- src/test/java/org/sreelakshmitest/Test002.java
+++ src/test/java/org/sreelakshmitest/Test002.java
@@ -1,7 +1,23 @@
 package org.sreelakshmitest;
 
+import io.restassured.RestAssured;
+
 public class Test002 {
     public static void main(String[] args) {
+        System.out.println("Rest Assured Test CASE");
+        System.out.println("GET Request Demo");
 
+        // Gherkins Syntax
+//        given() - url, headers, body or paylaod
+//        when() - http methods - get, post, patch, put, delete
+//        then() - verify the response - er == ar
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1").log().all()
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200); // <201> but was <200>.
     }
 }
